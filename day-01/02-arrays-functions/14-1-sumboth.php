4c866a0ce5453a1633266479960e98cd2dcd74be
deanssmart <deanssmart@gmail.com>
2020-08-03 16:50:42 +0100

    14th challenge completed SUMBOTH using foreach

---
+++ day-01/02-arrays-functions/14-1-sumboth.php
@@ -0,0 +1,27 @@
+<?php
+
+// Create a function, sumBoth, that takes two arrays of numbers as arguments. Return the sum of all the numbers from both arrays.
+
+// Hint: to avoid repeating code, you might want to write a sum function that adds up the values in a single array and call that inside sumBoth
+
+// Try it using foreach
+// Try it using collect()
+
+require __DIR__ . "/vendor/autoload.php";
+
+function sum($arr){
+    $total = 0;
+    foreach($arr as $val){
+        $total += $val;
+    }
+    return $total;
+}
+
+function sumBoth($arr1, $arr2) {
+    return sum($arr1) + sum($arr2);
+}
+
+dump(
+    sumBoth([2, 3, 4, 5, 6], [1, 2, 3, 4]), // 30
+    sumBoth([2, 3, 4, 5, 6], [5, 10, 12]), // 47
+);
