6bcce8f657684fb27bfab8333e5c3c2d91b433c4
deanssmart <deanssmart@gmail.com>
2020-08-03 11:57:43 +0100

    2nd challenge completed CONCATENATION and INTERPOLATION

---
+++ day-01/01-basic-php/concatenation.php
@@ -0,0 +1,16 @@
+<?php
+
+// Without changing the variable values, join the strings together with a space in between (create a separate file for each version):
+
+//     Using concatenation
+
+require __DIR__ . "/vendor/autoload.php";
+
+$string1 = "I";
+$string2 = "like";
+$string3 = "to";
+$string4 = "boogie";
+$string5 = "boogie";
+
+$joined = $string1 . " " . $string2 . " " . $string3 . " " . $string4 . " " . $string5;
+dump($joined); // "I like to boogie boogie"
