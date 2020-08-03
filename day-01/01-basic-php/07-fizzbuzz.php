23a65051b51e6497e2fec4a5bde1e5a1a43e40d0
deanssmart <deanssmart@gmail.com>
2020-08-03 12:49:06 +0100

    7th challenge completed FIZZBUZZ

---
+++ day-01/01-basic-php/07-fizzbuzz.php
@@ -0,0 +1,34 @@
+<?php
+
+// write some code that will output the numbers 1 to 100 in the console
+// unless the number is divisible by 3, in which case output "Fizz"
+// or the number is divisible by 5, in which case output "Buzz"
+// if the number is divisible by 3 and 5, output "FizzBuzz"
+// The first lines of output should look like this:
+
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// 16
+// ...
+// Make sure it's a nice elegant solution.
+
+require __DIR__ . "/vendor/autoload.php";
+
+for($i = 0; $i <= 100; $i += 1){
+    $str = $i % 3 === 0 ? "Fizz" : "";
+    $str .= $i % 5 === 0 ? "Buzz" : "";
+    dump($str === "" ? $i : $str);
+};
