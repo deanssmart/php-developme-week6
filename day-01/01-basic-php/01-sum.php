f134cec604fbfcb7131cccd0c981cf8c6111a673
deanssmart <deanssmart@gmail.com>
2020-08-03 11:51:32 +0100

    1st challenge SUM completed

---
+++ day-01/01-basic-php/01-sum.php
@@ -0,0 +1,13 @@
+<?php
+
+// Add up the numbers:
+
+require __DIR__ . "/vendor/autoload.php";
+
+$number1 = 10;
+$number2 = 20;
+$number3 = 30;
+$number4 = 40;
+
+$sum = $number1 + $number2 + $number3 + $number4;
+dump($sum); // 100
