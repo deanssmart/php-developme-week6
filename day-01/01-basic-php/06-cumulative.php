08fb910bf9a2ababa1381fad81899b3f3606041f
deanssmart <deanssmart@gmail.com>
2020-08-03 12:32:39 +0100

    6th challenge completed CUMULATIVE

---
+++ day-01/01-basic-php/06-cumulative.php
@@ -0,0 +1,20 @@
+<?php
+
+// Write some code that outputs a cumulative total of the current line number for 1 to 50.
+
+// e.g.
+
+// 1
+// 1 + 2
+// 1 + 2 + 3
+// 1 + 2 + 3 + 4
+// etc.
+
+require __DIR__ . "/vendor/autoload.php";
+
+$total = 0;
+
+for($i = 0; $i < 50; $i += 1){
+    dump($total += (1 + $i));
+};
+
