10e4f0f446cb10bcd1aed60fb91c8a3020e4906b
deanssmart <deanssmart@gmail.com>
2020-08-04 23:18:52 +0100

    3rd class challenge complete CAR

---
+++ day-02/02-classes/03-car.php
@@ -0,0 +1,53 @@
+<?php
+
+// Create a class that represents a car
+
+require __DIR__ . "/vendor/autoload.php";
+
+class Car
+{
+    private $make;
+    private $plate;
+    private $mileage = 0;
+
+    public function __construct($make, $plate)
+    {
+        $this->make = $make;
+        $this->plate = $plate;
+    }
+
+    public function getNumberPlate()
+    {
+        return $this->plate;
+    }
+
+    public function getMake()
+    {
+        return $this->make;
+    }
+
+    public function getMileage()
+    {
+        return $this->mileage;
+    }
+
+    public function addJourney($miles)
+    {
+        $this->mileage += $miles;
+    }
+}
+
+// you pass in a make and number plate
+$car = new Car("Ford", "XY11 4TY");
+
+// you can get various bits of information about it
+dump($car->getNumberplate()); // "XY11 4TY"
+dump($car->getMake()); // "Ford"
+dump($car->getMileage()); // 0
+
+// you can add journey
+$car->addJourney(100);
+dump($car->getMileage()); // 100
+
+$car->addJourney(200);
+dump($car->getMileage()); // 300
