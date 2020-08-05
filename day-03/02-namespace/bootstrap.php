14230e7628ef9d94e04b4ad304d35beb9eb159e1
deanssmart <deanssmart@gmail.com>
2020-08-05 17:11:41 +0100

    3rd namespace challenge completed POTATO

--- day-03/02-namespace/bootstrap.php
+++ day-03/02-namespace/bootstrap.php
@@ -28,3 +28,23 @@ $person2 = new Person("Wes", "Anderson");
 
 dump($person1->sayHelloTo($person2)); // "Hello Wes Anderson"
 dump($person2->sayHelloTo($person1)); // "Hello Lynne Ramsay"
+
+// Create a class Potato in the App\Stuff\Things namespace. It should have a water() and hasGrown() method. hasGrown() should return false until the Potato has been watered five or more times.
+
+echo "\nQuestion 3:\n";
+
+use App\Stuff\Things\Potato;
+
+$potato = new Potato();
+$potato->water()->water();
+
+dump($potato->hasGrown()); // false
+
+$potato->water()->water();
+dump($potato->hasGrown()); // false
+
+$potato->water();
+dump($potato->hasGrown()); // true
+
+$potato->water()->water();
+dump($potato->hasGrown()); // true
