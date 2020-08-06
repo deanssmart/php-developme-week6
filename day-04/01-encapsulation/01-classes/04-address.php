661195a44e00f6e2dfb9467c2a9d5ccc5ce75d90
deanssmart <deanssmart@gmail.com>
2020-08-06 13:19:20 +0100

    4th encapsulation challenge completed for ADDRESS

--- day-04/01-encapsulation/01-classes/04-address.php
+++ day-04/01-encapsulation/01-classes/04-address.php
@@ -3,7 +3,7 @@
 // Create a class that represents an address - use different properties for each part of the address. It should have a fullAddress method, which will return the full address as a nicely formatted string.
 
 // Hint: PHP has an implode() function, which is similar to join() in JS
-
+declare(strict_types=1);
 require __DIR__ . "/vendor/autoload.php";
 
 class Address
@@ -12,32 +12,32 @@ class Address
     private $postCode;
     private $town;
 
-    public function __construct($street, $postCode, $town)
+    public function __construct(string $street, string $postCode, string $town)
     {
         $this->street = $street;
         $this->postCode = $postCode;
         $this->town = $town;
     }
 
-    public function setStreet($street)
+    public function setStreet(string $street) : Address
     {
         $this->street = $street;
         return $this;
     }
 
-    public function setPostcode($postCode)
+    public function setPostcode(string $postCode) : Address
     {
         $this->postCode = $postCode;
         return $this;
     }
 
-    public function setTown($town)
+    public function setTown(string $town) : Address
     {
         $this->town = $town;
         return $this;
     }
 
-    public function fullAddress()
+    public function fullAddress() : string
     {
         return implode(", ", [$this->street, $this->town, $this->postCode]);
     }
